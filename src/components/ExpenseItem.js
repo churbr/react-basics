fd0725d9111aa7b81a85130a9b4630dfb4b772c2
chu.reybr <chu.reybr@gmail.com>
2023-10-06 01:59:28 +0800

    Added very first Component (ExpenseItem); Added devDependency: @babel/plugin-proposal-private-property-in-object

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,13 @@
+function ExpenseItem() {
+  return (
+    <div>
+      <div>July. 05, 2023</div>
+      <div>
+        <div>Dolce & Gabanna (Eau De Parfum) - The One</div>
+        <div>$138.55</div>
+      </div>
+    </div>
+  );
+}
+
+export default ExpenseItem;
