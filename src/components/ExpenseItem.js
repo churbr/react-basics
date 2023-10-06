be9fa88cfca77f53d76c68ef0c90387d2aa8f732
chu.reybr <chu.reybr@gmail.com>
2023-10-06 20:42:55 +0800

    Better way of passing & Receiving/Handling Props: Object Destructuring

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,14 +1,20 @@
 import "./ExpenseItem.css";
 
-function ExpenseItem(props) {
+function ExpenseItem({ item, price, date }) { // Object destructuring
+  const month = date.toLocaleString('en-US', { month: 'long' });
+  const day = date.toLocaleString('en-US', { day: '2-digit' });
+  const year = date.getFullYear();
+
   return (
     <div className="expense-item">
-      <div className="expense-item__description">
-        {props.date.toDateString()}
+      <div>
+        <div>{month}</div>
+        <div>{day}</div>
+        <div>{year}</div>
       </div>
       <div>
-        <div>{props.item}</div>
-        <div className="expense-item__price">${props.price}</div>
+        <div>{item}</div>
+        <div className="expense-item__price">${price}</div>
       </div>
     </div>
   );
