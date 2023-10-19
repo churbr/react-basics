56af5f912669adef815489fd40368e72182ea0d5
chu.reybr <chu.reybr@gmail.com>
2023-10-19 18:39:49 +0800

    Multiple states in one & Updating state from previous state (prevState)

--- src/components/NewExpenses/ExpenseForm.js
+++ src/components/NewExpenses/ExpenseForm.js
@@ -2,20 +2,41 @@ import { useState } from "react";
 import "./ExpenseForm.css";
 
 const ExpenseForm = () => {
-  const [title, setTitle] = useState("");
-  const [amount, setAmount] = useState("");
-  const [date, setDate] = useState("");
+  // const [title, setTitle] = useState("");
+  // const [amount, setAmount] = useState("");
+  // const [date, setDate] = useState("");
+
+  const [input, setInput] = useState({
+    title: '',
+    amount: '',
+    date: ''
+  });
 
   const titleChangeHandler = (e) => {
-    setTitle(e.target.value);
+    setInput((prevState) => {
+      return {
+        ...prevState,
+        title: e.target.value
+      }
+    });
   };
 
   const amountChangeHandler = (e) => {
-    setAmount(e.target.value);
+    setInput((prevState) => {
+      return {
+        ...prevState,
+        amount: e.target.value
+      }
+    });
   };
 
-  const dateChangeHandler = () => {
-    setDate(e.target.value);
+  const dateChangeHandler = (e) => {
+    setInput((prevState) => {
+      return {
+        ...prevState,
+        date: e.target.value
+      }
+    });
   };
 
   return (
@@ -23,13 +44,13 @@ const ExpenseForm = () => {
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" value={title} onChange={titleChangeHandler} />
+          <input type="text" value={input.title} onChange={titleChangeHandler} />
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
           <input
             type="number"
-            value={amount}
+            value={input.amount}
             min="0.01"
             step="0.01"
             onChange={amountChangeHandler}
@@ -39,7 +60,7 @@ const ExpenseForm = () => {
           <label>Date</label>
           <input
             type="date"
-            value={date}
+            value={input.date}
             min="2023-10-19"
             max="2024-10-19"
             onChange={dateChangeHandler}
